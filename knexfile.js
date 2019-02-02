e522a64071947f05ee3881bedd1170c92fd1ff01
kostyanius <kostyanius@hotmail.com>
2019-02-02 22:08:57 +0000

    adiing many changes

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,10 @@
 module.exports = {
   client: 'pg',
   connection: {
-    host : process.env.PG_HOST || 'postgres',
+    host : process.env.PG_HOST || 'postgresql.postgres.database.azure.com',
     port : process.env.PG_PORT || 5432,
-    user : process.env.PG_USER || 'postgres',
-    password : process.env.PG_PASSWORD || 'password',
+    user : process.env.PG_USER || "postgres",
+    password : process.env.PG_PASSWORD || 'kEYZXMjhEgQh34ZGHzk',
     database : process.env.PG_DATABASE || 'bookapi',
     charset  : 'utf8'
   }
