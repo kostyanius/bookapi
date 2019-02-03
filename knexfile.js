3ecbe2034213a36c00ed5e9866863498d1fdd2a2
kostyanius <kostyanius@hotmail.com>
2019-02-03 16:40:47 +0000

    adding folder web

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ module.exports = {
   connection: {
     host : process.env.PG_HOST || 'postgresql.postgres.database.azure.com',
     port : process.env.PG_PORT || 5432,
-    user : process.env.PG_USER || "postgres",
+    user : process.env.PG_USER || "postgres@postgresql",
     password : process.env.PG_PASSWORD || 'kEYZXMjhEgQh34ZGHzk',
     database : process.env.PG_DATABASE || 'bookapi',
     charset  : 'utf8'
